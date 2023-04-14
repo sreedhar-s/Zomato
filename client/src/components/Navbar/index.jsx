ff9e661da51cf51162003ab6a79df9f99aa6d091
sreedhar <gandikotasreedhar98@gmail.com>
2023-04-14 18:56:39 +0530

    fixed navbar width issue

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -71,39 +71,43 @@ const MediumNav = () => {
 const LargeNav = () => {
   return (
     <>
-      <div className="hidden lg:flex items-center justify-between gap-3 w-3/4">
-        <div className="w-28">
-          <img
-            src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
-            alt="logo"
-            className="w-full h-full"
-          />
-        </div>
-        <div className="w-full flex items-center gap-3 border border-gray-200 bg-white shadow-md p-3">
+      <div className="container px-20 mx-auto">
+        <div className="hidden lg:flex items-center justify-between gap-3">
+          <div className="w-28">
+            <img
+              src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+              alt="logo"
+              className="w-full h-full"
+            />
+          </div>
+          <div className="w-3/4 flex items-center gap-3 border border-gray-200 bg-white shadow-md p-3">
             <div className="flex items-center gap-2 border-r-2 pr-2 border-gray-300">
-                <span className="text-zomato-400 ">
-                    <HiLocationMarker />
-                </span>
-                <input type="text" placeholder="Guntakal" className="focus:outline-none" />
-                <IoMdArrowDropdown />
+              <span className="text-zomato-400 ">
+                <HiLocationMarker />
+              </span>
+              <input
+                type="text"
+                placeholder="Guntakal"
+                className="focus:outline-none"
+              />
+              <IoMdArrowDropdown />
             </div>
             <div className="flex w-full items-center gap-2">
-                <BiSearch />
-                <input
+              <BiSearch />
+              <input
                 type="search"
                 placeholder="Search for restaurant, cuisine or a dish"
                 className="w-full focus:outline-none"
               />
             </div>
+          </div>
+          <div className="ml-28 flex items-center gap-4">
+            <button className="text-gray-400 hover:text-gray-800">Login</button>
+            <button className="text-gray-400 hover:text-gray-800">
+              signup
+            </button>
+          </div>
         </div>
-        <div className="flex items-center gap-2">
-            <button className="text-gray-400 hover:text-gray-800">
-                Login
-            </button>
-            <button className="text-gray-400 hover:text-gray-800">
-                signup
-            </button>
-      </div>
       </div>
     </>
   );
