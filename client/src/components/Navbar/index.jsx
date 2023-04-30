1e868b078b9f747eb1711330d8d318daccdaae26
sreedhar <gandikotasreedhar98@gmail.com>
2023-04-30 12:40:10 +0530

    added signin and signup modals

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUserAlt } from "react-icons/fa";
-import {HiLocationMarker} from "react-icons/hi";
-import {IoMdArrowDropdown, IoMdArrowDropup} from "react-icons/io";
-import {BiSearch} from "react-icons/bi";
+import { HiLocationMarker } from "react-icons/hi";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { RiSearch2Line } from "react-icons/ri";
 
-const MobileNav = () => {
+//components
+import SignIn from "../Auth/signin";
+import SignUp from "../Auth/signup";
+
+const NavSm = ({ SignIn,SignUp }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <div className="flex items-center justify-between md:hidden">
+    <div className="flex w-full items-center justify-between lg:hidden">
       <div className="w-28">
         <img
           src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
@@ -14,65 +20,29 @@ const MobileNav = () => {
           className="w-full h-full"
         />
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 relative">
         <button className="bg-zomato-400 text-white py-2 px-3 rounded-full">
           Use App
         </button>
-        <span className="border p-2 border-gray-300 text-zomato-400 rounded-full">
+        <span onClick={() => setIsDropdownOpen(prev => !prev)} className="border p-2 border-gray-300 text-zomato-400 rounded-full">
           <FaUserAlt />
         </span>
+        {isDropdownOpen && (
+          <div className="absolute shadow-lg py-3 -bottom-20 -right-4 w-full bg-white z-20 flex flex-col gap-2">
+            <button onClick={SignIn}>Sign In</button>
+            <button onClick={SignUp}>Sign Up</button>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-const MediumNav = () => {
+const LargeNav = ({SignIn, SignUp}) => {
   return (
     <>
-      <div className="hidden md:flex w-full items-center justify-between gap-3 lg:hidden">
-        <div className="w-28">
-          <img
-            src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
-            alt="logo"
-            className="w-full h-full"
-          />
-        </div>
-        <div className="w-full flex items-center gap-3 border border-gray-200 bg-white shadow-md p-3">
-            <div className="flex items-center gap-2 border-r-2 pr-2 border-gray-300">
-                <span className="text-zomato-400 ">
-                    <HiLocationMarker />
-                </span>
-                <input type="text" placeholder="Guntakal" className="focus:outline-none" />
-                <IoMdArrowDropdown />
-            </div>
-            <div className="flex items-center gap-2">
-                <BiSearch />
-                <input 
-                    type="search" 
-                    placeholder="Search for restaurant, cuisine or a dish" 
-                    className="w-full focus:outline-none"
-                />
-            </div>
-        </div>
-        <div className="flex items-center gap-2">
-            <button className="text-gray-400 hover:text-gray-800">
-                Login
-            </button>
-            <button className="text-gray-400 hover:text-gray-800">
-                signup
-            </button>
-        </div>
-
-      </div>
-    </>
-  );
-};
-
-const LargeNav = () => {
-  return (
-    <>
-      <div className="container px-20 mx-auto">
-        <div className="hidden lg:flex items-center justify-between gap-3">
+      <div className="hidden container px-20 mx-auto lg:flex">
+        <div className="hidden gap-4 w-full items-center justify-around lg:flex">
           <div className="w-28">
             <img
               src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
@@ -80,20 +50,20 @@ const LargeNav = () => {
               className="w-full h-full"
             />
           </div>
-          <div className="w-3/4 flex items-center gap-3 border border-gray-200 bg-white shadow-md p-3">
-            <div className="flex items-center gap-2 border-r-2 pr-2 border-gray-300">
-              <span className="text-zomato-400 ">
+          <div className=" w-3/4 bg-white shadow-md p-3 flex items-center gap-3  border border-gray-200 rounded">
+            <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+              <span className="text-zomato-400">
                 <HiLocationMarker />
               </span>
               <input
                 type="text"
-                placeholder="Guntakal"
-                className="focus:outline-none"
+                placeholder="Bengaluru"
+                className=" focus:outline-none"
               />
               <IoMdArrowDropdown />
             </div>
             <div className="flex w-full items-center gap-2">
-              <BiSearch />
+              <RiSearch2Line />
               <input
                 type="search"
                 placeholder="Search for restaurant, cuisine or a dish"
@@ -101,10 +71,12 @@ const LargeNav = () => {
               />
             </div>
           </div>
-          <div className="ml-28 flex items-center gap-4">
-            <button className="text-gray-400 hover:text-gray-800">Login</button>
-            <button className="text-gray-400 hover:text-gray-800">
-              signup
+          <div className="ml-28 flex gap-4 ">
+            <button onClick={SignIn} className="text-gray-500 text-xl hover:text-gray-800">
+              Login
+            </button>
+            <button onClick={SignUp} className="text-gray-500 text-xl hover:text-gray-800">
+              Signup
             </button>
           </div>
         </div>
@@ -114,12 +86,18 @@ const LargeNav = () => {
 };
 
 const Navbar = () => {
+  const [openSignin, setOpenSignin] = useState(false);
+  const [openSignup, setOpenSignup] = useState(false);
+
+  const openSignInModal = () => setOpenSignin(true);
+  const openSignUpModal = () => setOpenSignup(true);
   return (
     <>
-      <nav className="p-4 bg-white">
-        <MobileNav />
-        <MediumNav />
-        <LargeNav />
+      <SignIn isOpen={openSignin} setIsOpen={setOpenSignin} />
+      <SignUp isOpen={openSignup} setIsOpen={setOpenSignup} />
+      <nav className="p-4 flex bg-white shadow-md lg:shadow-none w-full items-center">
+        <NavSm SignIn={openSignInModal} SignUp={openSignUpModal} />
+        <LargeNav SignIn={openSignInModal} SignUp={openSignUpModal} />
       </nav>
     </>
   );
